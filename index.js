5e07e494e26d39ca06e1f58289447ae1e89e27ea
Mariperu <maritzarg2020@gmail.com>
2021-07-30 07:02:55 -0500

    creando rama second e inst npm

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const chalk = require('chalk');
+console.log(chalk.red('Hello world!'));
+
+// //PROBANDO AL INFINITO
+// let i = 0;
+// setInterval(function () {
+//   console.log(i);
+//   i++;
+// }, 50);
