f7e23a3649a646ff8e04f2eb2603d6ffd3e2247b
Mariperu <maritzarg2020@gmail.com>
2021-08-24 23:17:19 -0500

    Modularizando código

--- src/api/filesLinks.js
+++ src/api/filesLinks.js
@@ -1,5 +1,10 @@
 //FUNCIONES PARA EXTRAER INFORMACION DE LINKS en array
 
+//TERMINAL: node ./src/api/filesLinks.js ./fixedPathFiles
+
+//Ejecutando módulo chalk, para colocar colores en líneas de comando
+//const chalk = require('chalk');
+
 //Ejecutando módulo marked, compilador de bajo nivel, liviano, sencillo
 //para analizar archivos markdown, construido para brindar velocidad
 //(ayuda a transformar md en html)
@@ -20,21 +25,24 @@ const fetch = require('node-fetch');
 const {
   readFileMd, //Lee un archivo en terminal
 } = require('./path');
+const {
+  searchFilesMd, //Devuelve array con todos los files.md
+} = require('./filesMd');
+
+//const myPath = process.argv[2];
 
 
 //VERIFICA SI archivo.md TIENE LINKS, Y GUARDANDO SUS PROPIEDADES {href, text, file} EN ARRAY***
-
-const linksOfFileMd = (arrayFilesMd) => {
-  const arrayOfLinksProperties = [];
+const linksOfFileMd = (myPath) => {
+  const arrayFilesMd = searchFilesMd(myPath);
+  const arrayLinksProperties = [];
   arrayFilesMd.forEach((fileMd) => {
-
     //** Pasando texto md a html **
     //marked.lexer(), crea una serie (array) de tokens, que pasará al marked.parser()
     //tokens: objetos {} con propiedades que describen cada parte de texto .md
     const tokens = marked.lexer(readFileMd(fileMd)); //obj describe c/linea del file
     //marked.parser(), procesa cada token en la matriz de tokens (para pasarlo a html).
     const html = marked.parser(tokens);
-
     //** RECREANDO DOM, se utiliza propiedad "window"
     const dom = new JSDOM(html);
     //Extrayendo links, seleccionando todas las etiquetas <a></a>
@@ -42,46 +50,65 @@ const linksOfFileMd = (arrayFilesMd) => {
 
     //Almacenando propiedades de cada link en un array de objetos [{}]
     extractingLinks.forEach((link) => {
-      arrayOfLinksProperties.push({ //Ejm: <a href="https://example.com">text</a>
-        href: link.href, //URL encontrada.
-        text: link.text, //Texto que aparecía dentro del link (<a>).
-        file: fileMd, //Ruta del archivo donde se encontró el link.
+      arrayLinksProperties.push({ //Ejm: <a href="https://example.com">text</a>
+        href: link.href,
+        text: link.text,
+        file: fileMd,
       });
     });
   });
-  return arrayOfLinksProperties;
+  return arrayLinksProperties;
 };
-//console.log("que hay en cada link?: ", linksOfFileMd([myPath]));
+//console.log("que hay en cada link?: ", linksOfFileMd(myPath));
 
 
-
-//*****ALMACENANDO STATUS DE LINKS {href, text, file, status, statusText} EN ARRAY***
-const linksStatusOfFileMd = (arrayLinks) => {
-  //console.log("esto ingresó: ", arrayLinks)
-  const arrayOfLinksStatus = [];
+//ALMACENANDO STATUS DE LINKS {href, text, file, status, statusText} EN ARRAY***
+const linksStatus = (arrayLinks) => {
+  const arrayLinksStatus = [];
   arrayLinks.forEach((link) => {
     //fetch(url) devuelve una promesa, promesa devuelve un objeto con datos del link
-    arrayOfLinksStatus.push(fetch(link.href)
-      .then((response) => { //Cuando promesa es resuelta, response es un callback //console.log("Resolve", { //Objeto
+    arrayLinksStatus.push(fetch(link.href)
+      .then((response) => { //Cuando promesa es resuelta, response es un callback
+        //console.log(response);
         if (response.status >= 200 && response.status < 400) {
-          return `${link.href}  ${link.text}  ${link.file}  ${response.status}  'ok'`;
+          //return `${chalk.magenta(link.file)} ${chalk.white(link.href)} ${chalk.cyanBright(link.text)} ${chalk.greenBright(response.status)} ${chalk.green('ok')}`;
+          return {
+            file: link.file,
+            href: link.href,
+            text: link.text,
+            status: response.status,
+            statusText: 'ok',
+          }
         } else {
-          return `${link.href}  ${link.text}  ${link.file}  ${response.status}  'fail'`;
+          //return `${chalk.magenta(link.file)} ${chalk.white(link.href)} ${chalk.cyanBright(link.text)} ${chalk.yellowBright(response.status)} ${chalk.yellow('fail')}`;
+          return {
+            file: link.file,
+            href: link.href,
+            text: link.text,
+            status: response.status,
+            statusText: 'fail',
+          }
         }
       })
-      .catch(() => { //Cuando promesa es rechazada //console.log("Rejected", { //Objeto
-        return `${link.href}  ${link.text}  ${link.file}  'error'  'fail'`;
+      .catch(() => { //Cuando promesa es rechazada //Rejected
+        //return `${chalk.magenta(link.file)} ${chalk.white(link.href)} ${chalk.cyanBright(link.text)} ${chalk.redBright('error fail')}`;
+        return {
+          file: link.file,
+          href: link.href,
+          text: link.text,
+          status: 'error',
+          statusText: 'fail',
+        }
       }));
   });
-  //Promise.all: método estático. Cuando se hayan ejecutado todas las peticiones se cumplirá la promesa.
-  return Promise.all(arrayOfLinksStatus);
-  //return arrayOfLinksStatus;
+  //Promise.all: método estático.
+  //Cuando se hayan ejecutado todas las peticiones se cumplirá la promesa.
+  return Promise.all(arrayLinksStatus);
 }
-// Promise.all(linksStatusOfFileMd(linksOfFileMd([myPath]))).then((resp) => console.log((resp)));
-//linksStatusOfFileMd(linksOfFileMd([myPath])).then((resp) => console.log((resp)));
+//linksStatus(linksOfFileMd(myPath)).then((resp) => console.log((resp)));
 
 
 module.exports = {
   linksOfFileMd, //Lee archivo.md, busca links <a> y almacena sus prop en un array
-  linksStatusOfFileMd, //Recibe prop de links, retorna promesas y almacena status de cada link en array
+  linksStatus, //Recibe prop de links, retorna promesas y almacena status de cada link en array
 };
--- src/api/path.js
+++ src/api/path.js
@@ -1,9 +1,16 @@
 //FUNCIONES PARA IDENTIFICAR RUTA / ARCHIVO / DIRECTORIOS-SUBDIRECTORIOS
 
-//Ejecutando módulo FileSystem, para interactuar con el sist. de archivos (viene implicito en package.json)
+//Ejecutando módulo FileSystem
+//para interactuar con el sist. de archivos (viene implícito en package.json)
 const fs = require('fs');
 //Ejecutando módulo path, contiene utilidades para trabajar con rutas de fichero
 const path = require('path');
+
+
+//process.argv[], matriz que contiene los argumentos de la línea de comando.
+//[0]y[1] se ignoran, son nativos de node.js. [2]: contiene el argumento.
+//ejm: ["node","file.js","<argument>",...]  =  node api ./fixedPathFiles
+//const myPath = process.argv[2];
 
 
 //VERIFICA SI EXISTE LA RUTA
@@ -14,51 +21,46 @@ const isPath = (myPath) => fs.existsSync(myPath);
 //VERIFICA Y MUESTRA RUTA ES ABSOLUTA (Si ruta es relativa transforma a abs)
 //método path.isAbsolute, **devuelve un booleano**
 //método path.resolve(), resuelve una secuencia o segmentos de ruta en una **ruta absoluta**
-const isPathAbsolute = (myPath) => (path.isAbsolute(myPath)) ?
-  myPath : path.resolve(myPath);
+const isPathAbsolute = (myPath) => path.isAbsolute(myPath) ? myPath : path.resolve(myPath);
 
 
 //VERIFICA SI RUTA ES ARCHIVO
 //método fs.statSync(),devuelve información de ruta
 //isFile(),verifica si es archivo, **devuelve booleano**
-const isPathFile = (pathAbs) => {
-  const stat = fs.statSync(pathAbs);
-  return stat.isFile();
-}
+const isPathFile = (myPath) => fs.statSync(myPath).isFile();
+
 
 //VERIFICA SI RUTA ES DIRECTORIO
 //método fs.statSync(),devuelve información de ruta
 //isDirectory(),verifica si es directorio, **devuelve booleano**
-const isPathDirectory = (pathAbs) => {
-  const stat = fs.statSync(pathAbs);
-  return stat.isDirectory();
-}
+//const isPathDirectory = (myPath) => fs.statSync(myPath).isDirectory();
+
 
 //MUESTRA EXTENSION DE LA RUTA
 //método path.extname(), verifica últ. aparición de punto(.), si es directorio devuelve nada
-const showFileExt = (file) => (path.extname(file));
+const showFileExt = (myPath) => (path.extname(myPath));
 
 
 //LEE DIRECTORIO
 //fs.readdirSync(path[,option]), devuelve un array con cada nombre de contenido (files y sub-directorios)
 //[, options] puede ser un encoding, por Ejm: 'utf-8'
 //utf-8: formato de codificación de caracteres Unicode e ISO 10646 que utiliza símbolos de longitud variable.
-const readDirectory = (pathAbs) => fs.readdirSync(pathAbs, 'utf-8');
+//const readDirectory = (dirPath) => fs.readdirSync(dirPath, 'utf-8');
+const readDirectory = (dirPath) => fs.readdirSync(dirPath);
 
 
 //LEE ARCHIVO en terminal
 //fs.readFileSync(path[, options]); si no se ingresa [, options], devolverá un buffer (binario)
-//[, options] puede ser un encoding, por Ejm: 'utf-8'
-//utf-8: formato de codificación de caracteres Unicode e ISO 10646 que utiliza símbolos de longitud variable.
-const readFileMd = (fileMd) => fs.readFileSync(fileMd, 'utf-8');
+//const readFileMd = (filePath) => fs.readFileSync(filePath).toString(); //convierte ruta a type string
+const readFileMd = (filePath) => fs.readFileSync(filePath, 'utf-8')
 
 
 module.exports = {
-  isPath, //Verifica si existe ruta
-  isPathAbsolute, //Verifica y transforma a ruta abs
-  isPathFile, //Verifica si es archivo
-  isPathDirectory, //Verifica si es directorio
-  showFileExt, //Muestra extensión de archivo
-  readDirectory, //Lee un directorio
-  readFileMd, //Lee un archivo en terminal
+  isPath,
+  isPathAbsolute,
+  isPathFile,
+  //isPathDirectory,
+  showFileExt,
+  readDirectory,
+  readFileMd,
 }
