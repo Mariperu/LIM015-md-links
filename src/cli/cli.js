32b4bc57a0646f6793c625465e197c751497c8a8
Mariperu <maritzarg2020@gmail.com>
2021-08-31 09:43:01 -0500

    agregando bin

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,11 +1,8 @@
-#! / usr / bin / env nodo
- //#!/usr/bin/env nodo
-//#!/usr/bin/env node
+#!/usr/bin/env nodo
+ //#!/usr/bin/env node  //Para que se ejecute el módulo con npm
 
-//Para que se ejecute el módulo con npm
-
-//CLI - Interfaz de Línea de Comando
-//TERMINAL: node ./src/cli/cli.js ./test/directory/file1.md --validate
+//CLI - INTERFAZ DE LINEA DE COMANDO
+//terminal: node ./src/cli/cli.js ./test/directory/file1.md --validate
 
 
 //Ejecutando módulo chalk, para colocar colores en líneas de comando
