ac442b88d2bf5fc0201339468615c78e65de0525
Mariperu <maritzarg2020@gmail.com>
2021-09-02 15:52:30 -0500

    Aplicando buenas practicas

--- src/api/index.js
+++ src/api/index.js
@@ -1,38 +1,20 @@
 //FUNCION MD-LINKS
-//Terminal: node ./src/api/index ./test/directory/file1.md
 
 const {
-  linksOfFileMd, //array
-  linksStatus, //array
+  linksOfFileMd,
+  linksStatus,
 } = require('./filesLinks');
 
-//const myPath = process.argv[2];
 
-//FUNCION PARA VALIDAR LINKS DE FILES .md
 const mdLinks = (myPath, options) => {
   const arrayLinks = linksOfFileMd(myPath);
   return new Promise((resolve) => {
-    //{validate:true}
     if (options.validate) {
-      resolve(linksStatus(arrayLinks)); //devuelve: Promise { <pending> }
-    }
-    //{validate:false} //else if (!options.validate) {
-    else {
+      resolve(linksStatus(arrayLinks));
+    } else {
       resolve(arrayLinks);
     }
   });
 }
 
-module.exports = mdLinks; //valida links
-
-
-// (mdLinks(myPath, {
-//   validate: true
-// })).then((res) => {
-//   console.log(res)
-// });
-// (mdLinks(myPath, {
-//   validate: false
-// })).then((res) => {
-//   console.log(res)
-// });
+module.exports = mdLinks;
